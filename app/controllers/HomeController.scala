0944ec9aa52b89375c65cedf066b775902d2a1bf
Dale Wijnand <dale.wijnand@gmail.com>
2019-07-23 18:21:10 +0100

    Tuesday progress

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,221 +1,90 @@
 package controllers
 
+import java.lang.reflect.Method
 import scala.reflect.ClassTag
-import akka.actor.{ Actor, ActorSystem, typed }
+import akka.actor.{ Actor, ActorSystem, Props, typed }
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
 import com.google.inject.assistedinject.{ Assisted, FactoryModuleBuilder }
-import com.google.inject.{ AbstractModule, TypeLiteral }
+import com.google.inject.binder.LinkedBindingBuilder
+import com.google.inject.name.Names
+import com.google.inject.{ AbstractModule, Binder, TypeLiteral }
+import com.google.inject.util.Providers
 import javax.inject._
 import play.api.Configuration
-import play.api.inject.Injector
+import play.api.inject.{ Injector, SimpleModule, bind }
 import play.api.libs.concurrent.{ AkkaGuiceSupport, InjectedActorSupport }
 import play.api.mvc._
 
-object HelloActor {
-  final case class SayHello(name: String, replyTo: ActorRef[String])
+// TODO: actor name
+// TODO: annotatedWith(Names.named(name))
+object Utils {
+  def lookupConf(conf: Configuration, key: String) = conf.getOptional[String](key).getOrElse("none")
+}; import Utils._
 
-  def apply(): Behavior[SayHello] = Behaviors.receiveMessage {
-    case SayHello(name, replyTo) =>
-      replyTo ! s"Hello, $name"
-      Behaviors.same
-  }
-}
-
-object ConfiguredActor {
-  final case class GetConfig(replyTo: ActorRef[String])
-
-  @Inject() def apply(conf: Configuration): Behavior[GetConfig] = {
-    val config = conf.getOptional[String]("my.config").getOrElse("none")
-    Behaviors.receiveMessage {
-      case GetConfig(replyTo) =>
-        replyTo ! config
-        Behaviors.same
-    }
-  }
-}
-
-object ConfiguredChildActor {
-  final case class GetConfig(replyTo: ActorRef[String])
-
-  @Inject() def apply(conf: Configuration, @Assisted key: String): Behavior[GetConfig] = {
-    val config = conf.getOptional[String](key).getOrElse("none")
-    Behaviors.receiveMessage {
-      case GetConfig(replyTo) =>
-        replyTo ! config
-        Behaviors.same
-    }
-  }
-
-  trait Factory {
-    def apply(key: String): Behavior[GetConfig]
-  }
-}
-
-object ParentActor {
-  final case class GetChild(key: String, replyTo: ActorRef[ActorRef[ConfiguredChildActor.GetConfig]])
-
-  @Inject() def apply(childFactory: ConfiguredChildActor.Factory): Behavior[GetChild] = Behaviors.receive {
-    case (ctx, GetChild(key, replyTo)) =>
-      val child: ActorRef[ConfiguredChildActor.GetConfig] = ctx.spawn(childFactory(key), key)
-      replyTo ! child
-      Behaviors.same
-  }
-}
-
-@Singleton
-final class TypedActorSystemProvider @Inject()(actorSystem: ActorSystem)
-    extends Provider[typed.ActorSystem[Nothing]] {
-  lazy val get = actorSystem.toTyped
-}
-
+@Singleton final class TypedActorSystemProvider @Inject()(system: ActorSystem) extends Provider[typed.ActorSystem[Nothing]] { lazy val get = system.toTyped }
+//class BuiltinModule2 extends SimpleModule((_, _) => Seq(bind[typed.ActorSystem[Nothing]].toProvider[TypedActorSystemProvider]))
 final class TypedActorSystemModule extends AbstractModule {
-  override def configure(): Unit = {
-    bind(new TypeLiteral[typed.ActorSystem[Nothing]]() {})
-        .toProvider(classOf[TypedActorSystemProvider])
-        .asEagerSingleton()
+  override def configure() = {
+    bind(new TypeLiteral[typed.ActorSystem[Nothing]] {}).toProvider(classOf[TypedActorSystemProvider]).asEagerSingleton()
   }
 }
 
-object FooActor {
-  final case class Event(name: String)
+final case class Event(name: String)
+final case class SayHello(name: String, replyTo: ActorRef[String])
+final case class GetConfig(replyTo: ActorRef[String])
+final case class GetChild(key: String, replyTo: ActorRef[ActorRef[GetConfig]])
+object           HelloActor {           def apply()                                           = { Behaviors.receiveMessage[SayHello]  { case SayHello(name, replyTo) => replyTo ! s"Hello, $name";                   Behaviors.same } } }
+object      ConfiguredActor { @Inject() def apply(conf: Configuration)                        = { Behaviors.receiveMessage[GetConfig] { case GetConfig(replyTo)      => replyTo ! lookupConf(conf, "my.config");     Behaviors.same } } }
+object ConfiguredChildActor { @Inject() def apply(conf: Configuration, @Assisted key: String) = { Behaviors.receiveMessage[GetConfig] { case GetConfig(replyTo)      => replyTo ! lookupConf(conf, key);             Behaviors.same } }; trait Factory { def apply(key: String): Behavior[GetConfig] } }
+object          ParentActor { @Inject() def apply(childFactory: ConfiguredChildActor.Factory) = { Behaviors.receive[GetChild]   { case (ctx, GetChild(key, replyTo)) => replyTo ! ctx.spawn(childFactory(key), key); Behaviors.same } } }
+object             FooActor {           def apply()                                           = { Behaviors.receiveMessage[Event]     { msg                          => println(s"foo => ${msg.name}");              Behaviors.same } } }
 
-  def apply(): Behavior[Event] =
-    Behaviors.receiveMessage { msg =>
-      println(s"foo => ${msg.name}")
-      Behaviors.same
-    }
-}
-
-class TypedActorRefProvider[T: ClassTag]() extends Provider[ActorRef[T]] {
-  @Inject private var actorSystem: ActorSystem = _
-  @Inject private var injector: Injector       = _
-
-  lazy val get: ActorRef[T] = {
-//    val creation = Props(injector.instanceOf[T])
-//    actorSystem.actorOf(props(creation), name)
-    ???
-  }
+class TypedActorRefProvider[T](behavior: Behavior[T]) extends Provider[ActorRef[T]] {
+  @Inject private var system: ActorSystem = _
+  lazy val get: ActorRef[T] = system.spawnAnonymous(behavior)
 }
 
 object TypedAkka {
-
-  def typedProviderOf[T: ClassTag]: Provider[ActorRef[T]] = new TypedActorRefProvider[T]
-
-//  /**
-//   * Create a binding for an actor implemented by the given class, with the given name.
-//   *
-//   * This will instantiate the actor using Play's injector, allowing it to be dependency injected itself.  The returned
-//   * binding will provide the ActorRef for the actor, qualified with the given name, allowing it to be injected into
-//   * other components.
-//   *
-//   * Example usage from a Play module:
-//   * {{{
-//   * def bindings = Seq(
-//   *   Akka.bindingOf[MyActor]("myActor"),
-//   *   ...
-//   * )
-//   * }}}
-//   *
-//   * Then to use the above actor in your application, add a qualified injected dependency, like so:
-//   * {{{
-//   *   class MyController @Inject() (@Named("myActor") myActor: ActorRef,
-//   *      val controllerComponents: ControllerComponents) extends BaseController {
-//   *     ...
-//   *   }
-//   * }}}
-//   *
-//   * @param name  The name of the actor.
-//   * @param props A function to provide props for the actor. The props passed in will just describe how to create the
-//   *              actor, this function can be used to provide additional configuration such as router and dispatcher
-//   *              configuration.
-//   * @tparam T The class that implements the actor.
-//   * @return A binding for the actor.
-//   */
-//  def bindingOf[T <: Actor: ClassTag](name: String, props: Props => Props = identity): Binding[ActorRef] =
-//    bind[ActorRef].qualifiedWith(name).to(providerOf[T](name, props)).eagerly()
+  def typedProviderOf[T](behavior: Behavior[T]) = new TypedActorRefProvider[T](behavior)
 }
 
 trait AkkaTypedGuiceSupport extends AkkaGuiceSupport { self: AbstractModule =>
-  import com.google.inject.util.Providers
-
-  def bindTypedActor[T: ClassTag]: Unit = {
-    binder
-        .bind(new TypeLiteral[ActorRef[T]]() {})
-        .toProvider(Providers.guicify(TypedAkka.typedProviderOf[T]))
-        .asEagerSingleton()
-  }
+  def bindTypedActor[T](tl: TypeLiteral[ActorRef[T]], behavior: Behavior[T]) = accessBinder.bind(tl).toProvider(Providers.guicify(TypedAkka.typedProviderOf[T](behavior))).asEagerSingleton()
+  private def accessBinder: Binder = { val method: Method = classOf[AbstractModule].getDeclaredMethod("binder"); if (!method.isAccessible) method.setAccessible(true); method.invoke(this).asInstanceOf[Binder] }
 }
 
-@Singleton final class FooActorProvider @Inject()(actorSystem: ActorSystem)
-    extends Provider[ActorRef[FooActor.Event]] {
-  def get() = actorSystem.spawn(FooActor(), "foo-actor")
-}
+@Singleton final class        FooActorProvider @Inject()(system: ActorSystem)                                             extends Provider[ActorRef[Event]]     { def get() = system.spawn(FooActor(), "foo-actor") }
+@Singleton final class ConfiguredActorProvider @Inject()(system: ActorSystem, conf: Configuration)                        extends Provider[ActorRef[GetConfig]] { def get() = system.spawn(ConfiguredActor(conf), "configured-actor") }
+@Singleton final class     ParentActorProvider @Inject()(system: ActorSystem, childFactory: ConfiguredChildActor.Factory) extends Provider[ActorRef[GetChild]]  { def get() = system.spawn(ParentActor(childFactory), "parent-actor") }
 
-@Singleton final class ConfiguredActorProvider @Inject()(
-    actorSystem: ActorSystem,
-    conf: Configuration,
-) extends Provider[ActorRef[ConfiguredActor.GetConfig]] {
-  def get() = actorSystem.spawn(ConfiguredActor(conf), "configured-actor")
-}
-
-@Singleton final class ParentActorProvider @Inject()(
-    actorSystem: ActorSystem,
-    childFactory: ConfiguredChildActor.Factory,
-) extends Provider[ActorRef[ParentActor.GetChild]] {
-  def get() = actorSystem.spawn(ParentActor(childFactory), "parent-actor")
-}
-
-final class FooActorModule extends AbstractModule with AkkaTypedGuiceSupport {
+final class AppModule extends AbstractModule with AkkaTypedGuiceSupport {
   override def configure(): Unit = {
-    bind(new TypeLiteral[ActorRef[FooActor.Event]]() {})
-        .toProvider(classOf[FooActorProvider])
-        .asEagerSingleton()
-    bind(new TypeLiteral[ActorRef[ConfiguredActor.GetConfig]]() {})
-        .toProvider(classOf[ConfiguredActorProvider])
-        .asEagerSingleton()
-    bind(new TypeLiteral[ActorRef[ParentActor.GetChild]]() {})
-        .toProvider(classOf[ParentActorProvider])
-        .asEagerSingleton()
-    bind(new TypeLiteral[ActorRef[ParentActor.GetChild]]() {})
-        .toProvider(classOf[ParentActorProvider])
-        .asEagerSingleton()
-    binder().install(
-      new FactoryModuleBuilder()
-          .implement(
-            new TypeLiteral[Behavior[ParentActor.GetChild]]() {},
-            new
-          )
-          .build(classOf[ConfiguredChildActor.Factory])
-    )
-
-    /*
-    bindActor[ParentActor]("parent-actor")
-    bindActorFactory[ConfiguredChildActor, ConfiguredChildActor.Factory]
-     */
-
+//    bind(new TypeLiteral[ActorRef[Event]]() {}).toProvider(classOf[FooActorProvider]).asEagerSingleton()
+    bindTypedActor(new TypeLiteral[ActorRef[Event]]() {}, FooActor())
+    bind(new TypeLiteral[ActorRef[GetConfig]]() {}).toProvider(classOf[ConfiguredActorProvider]).asEagerSingleton()
+//    bindTypedActor(ConfiguredActor(), "configured-actor")
+    // bindActor[ParentActor]("parent-actor")
+    // bind(new TypeLiteral[ActorRef[GetChild]]() {}).toProvider(classOf[ParentActorProvider]).asEagerSingleton()
+    // bindActorFactory[ConfiguredChildActor, ConfiguredChildActor.Factory]
+    // binder().install(new FactoryModuleBuilder().implement(new TypeLiteral[Behavior[GetChild]]() {}, ???).build(classOf[ConfiguredChildActor.Factory]))
   }
 }
 
 @Singleton class HomeController @Inject()(
+    conf: Configuration,
     cc: ControllerComponents,
-    actorSystem: ActorSystem,
+    system: ActorSystem,
     typedActorSystem: typed.ActorSystem[Nothing],
-    fooActor: ActorRef[FooActor.Event],
-    configuredActor: ActorRef[ConfiguredActor.GetConfig],
+    fooActor: ActorRef[Event],
+    configuredActor: ActorRef[GetConfig],
 ) extends AbstractController(cc) {
-  val helloActor: ActorRef[HelloActor.SayHello] = actorSystem.spawn(HelloActor(), "hello-actor")
+  val helloActor: ActorRef[SayHello] = system.spawn(HelloActor(), "hello-actor")
 
-  def action = Action {
-    fooActor.tell(FooActor.Event("a message"))
-    Ok("actors were told a little secret")
-  }
-}
+  val compileDi__configuredActor: ActorRef[GetConfig] =
+    system.spawn(ConfiguredActor(conf), "compile-di__configured-actor")
 
-@Singleton
-class Application @Inject()(system: ActorSystem, cc: ControllerComponents) extends AbstractController(cc) {
-
-
+  def action = Action { fooActor.tell(Event("a message")); Ok("actors were told a little secret") }
 }
